f6920e4f78812afee615431d609d8157c6e812d8
Coolindy <56873838+Chotiwat-Jeamjeang@users.noreply.github.com>
2026-02-27 17:45:33 +0700

    Update ai_analyzer.py

--- ai_analyzer.py
+++ ai_analyzer.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 def analyze_weather(weather_info):
 
